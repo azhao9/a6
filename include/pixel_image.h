322c22ddaec0b33d522ac6a41127d3f934969128
Aleck Zhao <aleck.zhao1114@gmail.com>
2016-10-14 17:13:38 -0400

    added header for pixel and image structs

---
+++ include/pixel_image.h
@@ -0,0 +1,24 @@
+/* pixel_image.h
+ *
+ * Aleck Zhao azhao9
+ * Noah Halpern nhalper1
+ * 14 October, 2016
+ *
+ * Defines structs for Pixels and Images
+ */
+
+struct Pixel_t {
+	unsigned char r;
+	unsigned char g;
+	unsigned char b;
+};
+
+typedef struct Pixel_t Pixel;
+
+struct Image_t {
+	Pixel* im;
+	int rows;
+	int cols;
+};
+
+typedef struct Image_t Image;
---
+++ include/ppmIO.h
@@ -0,0 +1,14 @@
+/* ppmIO.h
+ *
+ * Aleck Zhao azhao9
+ * Noah Halpern nhalper1
+ * 14 October 2016
+ *
+ * Headers for reading and writing images
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
