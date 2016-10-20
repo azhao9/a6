b04b129052648f79a3b9129577a3a98f47193e57
Noah Halpern <Noah@Noahs-MacBook-Pro-2.local>
2016-10-20 16:52:15 -0400

    wrote helper function and brightness adjustment

--- src/imageManip.c
+++ src/imageManip.c
@@ -14,119 +14,119 @@
 
 // crops an image
 Image* crop(Image *old) {
-	char a[30];
-	char b[30];
-	char c[30];
-	char d[30];
+    char a[30];
+    char b[30];
+    char c[30];
+    char d[30];
 
-	// reads coordinates
-	scanf("%s %s %s %s", a, b, c, d);
+    // reads coordinates
+    scanf("%s %s %s %s", a, b, c, d);
 
-	int x1 = atoi(a);
-	int y1 = atoi(b);
-	int x2 = atoi(c);
-	int y2 = atoi(d);
+    int x1 = atoi(a);
+    int y1 = atoi(b);
+    int x2 = atoi(c);
+    int y2 = atoi(d);
 
-	if (x2 < x1 || y2 < y1) {
-		// not properly formatted input
-		fprintf(stderr, "improper order of coordinates\n");
-		return NULL;
-	}
+    if (x2 < x1 || y2 < y1) {
+        // not properly formatted input
+        fprintf(stderr, "improper order of coordinates\n");
+        return NULL;
+    }
 
-	Pixel *oldPix = (*old).pixels;
-	int oldCols = (*old).cols;
-	int oldRows = (*old).rows;
+    Pixel *oldPix = (*old).pixels;
+    int oldCols = (*old).cols;
+    int oldRows = (*old).rows;
 
-	if (x1 < 0 || x2 < 0 || y1 < 0 || y2 < 0) {
-		// can't be less than 0
-		fprintf(stderr, "Coordinates must be positive\n");
-		return NULL;
-	}
+    if (x1 < 0 || x2 < 0 || y1 < 0 || y2 < 0) {
+        // can't be less than 0
+        fprintf(stderr, "Coordinates must be positive\n");
+        return NULL;
+    }
 
-	if (x1 > oldCols || x2 > oldCols || y1 > oldRows || y2 > oldRows) {
-		// can't be larger than size of picture
-		fprintf(stderr, "Can't be outside the picture\n");
-		return NULL;
-	}
+    if (x1 > oldCols || x2 > oldCols || y1 > oldRows || y2 > oldRows) {
+        // can't be larger than size of picture
+        fprintf(stderr, "Can't be outside the picture\n");
+        return NULL;
+    }
 
-	if ((strcmp(a, "0") && !x1) || (strcmp(b, "0") && !y1) || 
-			(strcmp(c, "0") && !x2) || (strcmp(d, "0") && !y2)) {
-	       // invalid input
-	       fprintf(stderr, "Invalid input\n");
-	       return NULL;
-	}	       
+    if ((strcmp(a, "0") && !x1) || (strcmp(b, "0") && !y1) || 
+            (strcmp(c, "0") && !x2) || (strcmp(d, "0") && !y2)) {
+        // invalid input
+        fprintf(stderr, "Invalid input\n");
+        return NULL;
+    }	       
 
-	int newRows = (y2 - y1);
-	int newCols = (x2 - x1);
+    int newRows = (y2 - y1);
+    int newCols = (x2 - x1);
 
-	// allocate a new pixel array
-	Pixel *newPix = malloc(sizeof(Pixel) * newRows * newCols);
+    // allocate a new pixel array
+    Pixel *newPix = malloc(sizeof(Pixel) * newRows * newCols);
 
-	int newIndex = 0;
+    int newIndex = 0;
 
-	printf("Cropping region from (%d, %d) to (%d, %d)...\n", x1, y1, x2, y2);
-	for (int r = y1; r < y2; r++) {
-		for (int c = x1; c < x2; c++) {
-			int oldIndex = r * oldCols + c;
+    printf("Cropping region from (%d, %d) to (%d, %d)...\n", x1, y1, x2, y2);
+    for (int r = y1; r < y2; r++) {
+        for (int c = x1; c < x2; c++) {
+            int oldIndex = r * oldCols + c;
 
-			newPix[newIndex] = oldPix[oldIndex];
-			newIndex++;
-		}
-	}
+            newPix[newIndex] = oldPix[oldIndex];
+            newIndex++;
+        }
+    }
 
-	Image* new = malloc(sizeof(Image));
+    Image* new = malloc(sizeof(Image));
 
-	(*new).pixels = newPix;
-	(*new).rows = newRows;
-	(*new).cols = newCols;
+    (*new).pixels = newPix;
+    (*new).rows = newRows;
+    (*new).cols = newCols;
 
-	// frees old image and pixels
-	free(oldPix);
-	free(old);
-	oldPix = NULL;
-	old = NULL;
+    // frees old image and pixels
+    free(oldPix);
+    free(old);
+    oldPix = NULL;
+    old = NULL;
 
-	return new;
+    return new;
 }
 
 // inverts an image
 void invert(Image *old) {
-	Pixel *pix = (*old).pixels;
-	int rows = (*old).rows;
-	int cols = (*old).cols;
+    Pixel *pix = (*old).pixels;
+    int rows = (*old).rows;
+    int cols = (*old).cols;
 
-	printf("Inverting intensity...\n");
-	for (int r = 0; r < rows; r++) {
-		for (int c = 0; c < cols; c++) {
-			int index = r * cols + c;
-			
-			// bitwise inverse
-			pix[index].red = ~pix[index].red;
-			pix[index].green = ~pix[index].green;
-			pix[index].blue = ~pix[index].blue;
-		}
-	}
+    printf("Inverting intensity...\n");
+    for (int r = 0; r < rows; r++) {
+        for (int c = 0; c < cols; c++) {
+            int index = r * cols + c;
+
+            // bitwise inverse
+            pix[index].red = ~pix[index].red;
+            pix[index].green = ~pix[index].green;
+            pix[index].blue = ~pix[index].blue;
+        }
+    }
 
 }
 
 // swaps color channels of an image
 void swap(Image *old) {	
-	Pixel *pix = (*old).pixels;
-	int rows = (*old).rows;
-	int cols = (*old).cols;
+    Pixel *pix = (*old).pixels;
+    int rows = (*old).rows;
+    int cols = (*old).cols;
 
-	printf("Swapping color channels...\n");
-	for (int r = 0; r < rows; r++) {
-		for (int c = 0; c < cols; c++) {
-			int index = r * cols + c;
+    printf("Swapping color channels...\n");
+    for (int r = 0; r < rows; r++) {
+        for (int c = 0; c < cols; c++) {
+            int index = r * cols + c;
 
-			unsigned char temp = pix[index].red;
+            unsigned char temp = pix[index].red;
 
-			pix[index].red = pix[index].green;
-			pix[index].green = pix[index].blue;
-			pix[index].blue = temp;
-		}
-	}
+            pix[index].red = pix[index].green;
+            pix[index].green = pix[index].blue;
+            pix[index].blue = temp;
+        }
+    }
 }
 
 // converts an image to grayscale
@@ -156,32 +156,64 @@ void grayscale(Image *old) {
 
 // changes the brightness of an image
 void brightness(Image *old) {
-	old = NULL;
-	if (old == NULL) {
-		printf("Feature not yet implemented\n");
-	}
+    char a[30];
+
+    scanf("%s", a);
+
+    unsigned char adj = (unsigned char)(atoi(a));
+
+    Pixel *pix = (*old).pixels;
+    int rows= (*old).rows;
+    int cols = (*old).cols;
+
+    for (int r = 0; r < rows; r++){
+        for (int c = 0; c < cols; c++){
+            int index = r * cols + c;
+
+            pix[index].red = sat(pix[index].red + adj);
+            pix[index].green = sat(pix[index].green + adj);
+            pix[index].blue = sat(pix[index].blue + adj);
+        }
+    }
+
 }
 
 // changes the contrast of an image
 void contrast(Image *old) {
-	old = NULL;
-	if (old == NULL) {
-		printf("Feature not yet implemented\n");
-	}
+    old = NULL;
+    if (old == NULL) {
+        printf("Feature not yet implemented\n");
+    }
 }
 
 // blurs an image
 void blur(Image *old) {
-	old = NULL;
-	if (old == NULL) {
-		printf("Feature not yet implemented\n");
-	}
+    old = NULL;
+    if (old == NULL) {
+        printf("Feature not yet implemented\n");
+    }
 }
 
 // sharpens an image
 void sharpen(Image *old) {
-	old = NULL;
-	if (old == NULL) {
-		printf("Feature not yet implemented\n");
-	}
+    old = NULL;
+    if (old == NULL) {
+        printf("Feature not yet implemented\n");
+    }
 }
+
+
+// saturation helper function
+unsigned char sat(unsigned char num){
+
+    if(num>255){
+        num = 255;
+    }
+    else if(num<0){
+        num = 0;
+    }
+
+    return (unsigned char)num;
+
+}
+
